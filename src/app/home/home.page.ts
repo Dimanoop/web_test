4cde594f6a0fbf70152ae5dd87a8cb6bd590e418
Maksimov24 <59806458+Dimanoop@users.noreply.github.com>
2025-10-04 10:07:05 +0000

    feat button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-language:string[] = ["Русский", "English"]
+  languages:string[] = ["Русский", "English"]
   constructor() {}
 
+  Myname: string = "Dima"
+  btn_click(){
+    this.Myname = this.Myname + "1"
+  }
 }
