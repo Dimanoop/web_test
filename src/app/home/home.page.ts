fe9bbeff2bf9cba63cc499b76f0928fd384851ba
Maksimov24 <59806458+Dimanoop@users.noreply.github.com>
2025-10-11 10:01:55 +0000

    add table

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
+interface Country{
+  name: string
+  population: number
+}
 
 @Component({
+  
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
 export class HomePage {
+
   languages:string[] = ["Русский", "English"]
   constructor() {}
-
   Myname: string = "Dima"
   btn_click(){
     this.Myname = this.Myname + "1"
