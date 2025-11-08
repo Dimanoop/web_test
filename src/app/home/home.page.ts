352732f28d34f1659ef7043490856e2941c5b33c
Maksimov24 <59806458+Dimanoop@users.noreply.github.com>
2025-11-08 08:04:42 +0000

    add logic button add city and popylation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,48 @@
 import { Component } from '@angular/core';
 
-interface Country{
+interface Country {
   name: string
   population: number
 }
 
 @Component({
-  
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: false,
-
 })
 export class HomePage {
   
-  languages:string[] = ["Русский", "English"]
-  countries: Country[]=[
+  languages: string[] = ["Русский", "English"]
+  countries: Country[] = [
     {name: "Moscow", population: 20000},
     {name: "Domodedovo", population: 5000},
-    {name: "Stypino", population: 3000}
+    {name: "Stupino", population: 3000}
   ] 
-  constructor() {
+  
+  constructor() {}
 
-  }
   isDarkTheme = false;
-
   Myname: string = "Dima"
-  btn_click(){
-    this.Myname = this.Myname + "1"
+  input_city: string = ""
+  input_population: number | null = null
+
+  btn_click() {
+    this.Myname = this.Myname + this.input_city;
+  }
+
+  addCity() {
+    if (this.input_city.trim() && this.input_population && this.input_population > 0) {
+      const newCountry: Country = {
+        name: this.input_city.trim(),
+        population: this.input_population
+      };
+      
+      this.countries.push(newCountry);
+      
+
+      this.input_city = "";
+      this.input_population = null;
+    }
   }
 }
- 
